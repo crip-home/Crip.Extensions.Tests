0f9c49e85adc05b54dec1bff683dfbc6c24027cd
Igors Krasjukovs <tahq69@users.noreply.github.com>
2022-08-25 15:06:13 +0300

    Add methods from a logging library

---
+++ src/Crip.Extensions.Tests/TestHttpHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Crip.Extensions.Tests;
+
+/// <summary>
+/// DelegatingHandler test implementation.
+/// </summary>
+public class TestHttpHandler : DelegatingHandler
+{
+    private readonly HttpResponseMessage _returnMessage;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TestHttpHandler"/> class.
+    /// </summary>
+    /// <param name="returnMessage">The response message to return on execution.</param>
+    public TestHttpHandler(HttpResponseMessage returnMessage)
+    {
+        _returnMessage = returnMessage;
+    }
+
+    /// <inheritdoc />
+    protected override Task<HttpResponseMessage> SendAsync(
+        HttpRequestMessage request,
+        CancellationToken ct)
+    {
+        return Task.Factory.StartNew(() => _returnMessage, ct);
+    }
+}
